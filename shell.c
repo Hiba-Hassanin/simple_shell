86ec6fcd4d9894feb432634e4567fc65aab0b426
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-12-16 03:16:07 +0200

    shell task

--- shell.c
+++ shell.c
@@ -1,58 +1,6 @@
 #include "shell.h"
 
 /**
- * main - Entry point of the shell program
- *
- * Return: Always 0
- */
-int main(int argc, char **argv)
-{
-	char command[MAX_COMMAND_LENGTH];
-
-	if (argc == 1)
-	{
-		while (1)
-		{
-			printf("($) ");
-			fgets(command, sizeof(command), stdin);
-			command[strcspn(command, "\n")] = '\0';
-			if (strcmp(command, "exit") == 0)
-				break;
-			else if (strcmp(command, "env") == 0)
-				system("env");
-			else
-				execute_command(command);
-		}
-	}
-	else if (argc == 2)
-	{
-		FILE *file = fopen(argv[1], "r");
-		char line[MAX_COMMAND_LENGTH];
-
-		if (!file)
-		{
-			perror("fopen");
-			exit(EXIT_FAILURE);
-		}
-
-		while (fgets(line, sizeof(line), file))
-		{
-			line[strcspn(line, "\n")] = '\0';
-			execute_command(line);
-		}
-
-		fclose(file);
-	}
-	else
-	{
-		fprintf(stderr, "Usage: %s [script_file]\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-
-	return (0);
-}
-
-/**
  * execute_command - Execute the given command
  * @command: The command to be executed
  */
