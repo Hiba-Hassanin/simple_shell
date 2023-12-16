d98bc6055298395a60eb67d019060908b70891ef
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-12-16 02:39:18 +0200

    shell task

--- shell.c
+++ shell.c
@@ -3,42 +3,35 @@
 /**
  * display_prompt - Displays the shell prompt.
  */
-
 void display_prompt(void)
 {
-	printf("$ ");
+	printf("#cisfun$ ");
+}
+
+/**
+ * read_command - Reads a command from stdin.
+ * @command: The buffer to store the command.
+ *
+ * Return: 1 on success, 0 on failure or end of file.
+ */
+int read_command(char *command)
+{
+	if (fgets(command, MAX_INPUT_LENGTH, stdin) == NULL)
+		return 0;
+
+	command[strcspn(command, "\n")] = '\0'; // Remove the trailing newline character
+
+	return (1);
 }
 
 /**
  * execute_command - Executes the given command.
  * @command: The command to be executed.
  */
-
 void execute_command(char *command)
 {
-	system(command);
-}
-
-/**
- * parse_command - Parses the command line and separates the command and arguments.
- * @input: The input command line.
- * @command: The buffer to store the command.
- * @arguments: The array to store the arguments.
- */
-
-void parse_command(char *input, char *command, char *arguments[])
-{
-	char *token;
-	int arg_index = 0;
-
-	token = strtok(input, " \t\n");
-	strcpy(command, token);
-
-	while (token != NULL)
-	{
-		arguments[arg_index++] = token;
-		token = strtok(NULL, " \t\n");
-	}
-
-	arguments[arg_index] = NULL;
+	if (access(command, X_OK) == 0)
+		system(command);
+	else
+		fprintf(stderr, "%s: command not found\n", command);
 }
--- shell.h
+++ shell.h
@@ -1,15 +1,15 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#define MAX_INPUT_LENGTH 1024
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-#define MAX_INPUT_LENGTH 1024
-
-void display_prompt();
+void display_prompt(void);
+int read_command(char *command);
 void execute_command(char *command);
-void parse_command(char *input, char *command, char *arguments[]);
 
 #endif
